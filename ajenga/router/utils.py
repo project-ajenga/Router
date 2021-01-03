5db530be7fdd4f42a4a0efc95694bd12f5a045ed
Hieuzest <girkirin@hotmail.com>
2021-01-03 21:27:44 +0800

    move max_instances to core

--- ajenga/router/utils.py
+++ ajenga/router/utils.py
@@ -136,19 +136,3 @@ async def as_completed(*coroutines: Coroutine,
                     yield await task
         else:
             break
-
-
-def max_instances(number, ignore=False, ignore_func=None):
-    def deco(func):
-        _sem = asyncio.Semaphore(number)
-
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            if ignore and _sem.locked():
-                return await ignore_func(*args, **kwargs) if ignore_func else None
-            async with _sem:
-                return await func(*args, **kwargs)
-
-        return wrapper
-
-    return deco
